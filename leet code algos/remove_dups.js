4963bd47ce24851b75ec4adbfe7b3a07397c9c30
bmlam-89 <bmlam89@gmail.com>
2018-02-20 19:07:47 -0800

    added remove duplicates to leetcode problems

---
+++ leet code algos/remove_dups.js
@@ -0,0 +1,13 @@
+//inplace removal of dups in a sorted array
+var removeDuplicates = function(nums) {
+    if(nums.length == 0)
+        return 0;
+    var i = 0;
+    for(var j = 1; j < nums.length; j++){
+        if(nums[i]!= nums[j]){
+            i++;
+            nums[i] = nums[j];
+        }
+    }
+    return i+1;
+}
