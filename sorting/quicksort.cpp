29a7cf7567d10e59603202f0ab4d273b1030ab5b
bmlam-89 <bmlam89@gmail.com>
2018-03-20 18:20:15 -0700

    added c++ implementation of quicksort

--- sorting/quicksort.cpp
+++ sorting/quicksort.cpp
@@ -1,18 +1,33 @@
 #include <iostream>
 using namespace std;
 
+void swap(int *xp, int *yp){
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
 
-void insertionsort(int arr[], int len){
-    int i,j,key;
-    for(i = 1; i < len; i++){
-        key = arr[i];
-        j = i-1;
-
-        while(j >= 0 && arr[j] > key){
-            arr[j+1] = arr[j];
-            j--;
+int partition(int arr[], int low, int high){
+    int pivot = arr[high];
+    int i = low-1;
+    for (int j = low; j <= high- 1; j++)
+    {
+        
+        if (arr[j] <= pivot)
+        {
+            i++;
+            swap(&arr[i], &arr[j]);
         }
-        arr[j+1] = key;
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
+}
+
+void quicksort(int arr[], int low, int high){
+    if(low < high){
+        int p_idx = partition(arr,low,high);
+        quicksort(arr,low,p_idx-1);
+        quicksort(arr,p_idx+1, high);
     }
 }
 
@@ -26,7 +41,6 @@ void print(int arr[], int len){
 
 int main(){
     int arr[] = {6,5,3,1,8,7,2,4};
-    int len = 5;
-    insertionsort(arr,len);
-    print(arr,len);
+    quicksort(arr,0,8);
+    print(arr,8);
 }
